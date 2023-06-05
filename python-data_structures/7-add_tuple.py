d08466ed4f3252de30456c775e5cf84a56e1a36f
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-04 22:08:03 -0700

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,10 +3,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Pad the tuples with zeros if they are smaller than 2
     tuple_a += (0, 0)[:2 - len(tuple_a)]
     tuple_b += (0, 0)[:2 - len(tuple_b)]
-    
     # Compute the sum of the elements in the tuples
     sum_first = tuple_a[0] + tuple_b[0]
-    sum_second = tuple_a[1] + tuple_b[1]
-    
+    sum_second = tuple_a[1] + tuple_b[1]    
     # Return a new tuple with the sums
     return (sum_first, sum_second)
