cf18c74ef5540b5bcb3ed78863994dad1e9bfee1
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-07 22:02:54 -0700

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
