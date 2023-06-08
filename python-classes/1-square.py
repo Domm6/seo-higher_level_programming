2f4acbe7232ddb7967bec607d2c394bbbac355a5
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-07 22:09:24 -0700

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,6 +4,9 @@
 This module defines the Square class that represents a square.
 """
 class Square:
-
+    
+    """
+    This method creates an instance of the method with a size
+    """
     def __init__(self, size):
         self.__size = size
