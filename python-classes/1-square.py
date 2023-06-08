30fe983e204a8bffbb2d32987caf3da7d87b8de6
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-07 22:14:10 -0700

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-"""
-This module defines the Square class that represents a square.
-"""
-
-
 class Square:
     """
-    This method creates an instance of the method with a size
+    This class represents a square.
     """
-    def __init__(self, size):
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
