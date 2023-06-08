a7c8640c1a0c4eaa0fe15951b45a63b275f59501
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-07 21:57:17 -0700

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
 """
 This module defines the Square class.
 """
+
 
 class Square:
     """
     This class represents a square.
     """
+
     def __init__(self):
         """
         Initializes a new instance of the Square class.
