cccf62c2a1424db39d8af65f50d293f007e19366
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-07 22:18:07 -0700

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class that represents a square.
+"""
+
+
+class Square:
     """
     This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
     """
-  
-  
-class Square:
 
     def __init__(self, size=0):
         """
@@ -12,6 +18,7 @@ class Square:
 
         Args:
             size (int): The size of the square. Default is 0.
+
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
