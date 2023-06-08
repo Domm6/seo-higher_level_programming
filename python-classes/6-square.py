ea809c3bae36401f2e9d9fab2fc45baf18d549af
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-08 16:39:31 -0700

    Update 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -18,10 +18,6 @@ class Square:
         Args:
             size (int): The size of the square. Default is 0.
             position (tuple): The position of the square. Default is (0, 0).
-
-        Raises:
-            TypeError: If size is not an integer or position is not a tuple of 2 positive integers.
-            ValueError: If size is less than 0 or any value in position is less than 0.
         """
         self.size = size
         self.position = position
