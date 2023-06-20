362498533cfb802b8a69a8d59b553f2d27f72ccd
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-20 10:26:49 -0700

    Create  0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Read MySQL credentials from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SQL query to fetch all states
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all rows from the result set
+    results = cursor.fetchall()
+
+    # Display the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
