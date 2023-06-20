15242c3596cfa4d7ca091abe6f18e58d3ad1de40
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-20 10:33:39 -0700

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, 
+    db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password, db=database)
 
     # Create a cursor object to interact with the database
