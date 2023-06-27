172bab14f1fa5381597e55502d68b0243a324083
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-26 18:16:30 -0700

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
-"""Script that lists all states"""
+"""
+Module: list_states_starting_with_N
+"""
 
 import MySQLdb
 import sys
 
-def search_states(username, password, database, state_name):
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+if __name__ == "__main__":
+    # Read MySQL credentials from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # Create a cursor object to execute queries
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object to interact with the database
     cursor = db.cursor()
 
-    # Create the SQL query using the provided state name
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    # Execute the SQL query to fetch states
+    # starting with 'N' (case-insensitive)
+    cursor.execute("SELECT * FROM states "
+                   "WHERE BINARY name LIKE 'N%' ORDER BY id ASC".format(state_name)
 
-    try:
-        # Execute the query
-        cursor.execute(query)
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()
 
-        # Fetch all the rows returned by the query
-        rows = cursor.fetchall()
+    # Display the results
+    for row in rows:
+        print(row)
 
-        # Display the results
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error executing the query:", e)
-        sys.exit(1)
-
-    finally:
-        # Close the database connection and cursor
-        cursor.close()
-        db.close()
-
-# Get the command line arguments
-if len(sys.argv) != 5:
-    print("Usage: python script.py <username> <password> <database> <state_name>")
-    sys.exit(1)
-
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-state_name = sys.argv[4]
-
-# Call the search_states function with the provided arguments
-search_states(username, password, database, state_name)
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
