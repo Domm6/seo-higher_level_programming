ec2004df0ded115e5efa3a19cf4424bc5a52f2e2
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-26 18:21:40 -0700

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-Module: list_states_starting_with_N
-
-"""
+"""Script that displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     # Read MySQL credentials from command-line arguments
