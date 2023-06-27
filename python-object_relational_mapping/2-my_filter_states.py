203f18f61ab0f89698db436716ec920203771f2b
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-26 18:22:10 -0700

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,9 +5,8 @@ table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    # Read MySQL credentials from command-line arguments
+    # Get command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -22,18 +21,17 @@ if __name__ == "__main__":
         db=database
     )
 
-    # Create a cursor object to interact with the database
+    # Create a cursor object
     cursor = db.cursor()
 
-    # Execute the SQL query to fetch states
-    # starting with 'N' (case-insensitive)
-    query.execute("SELECT * FROM states " \
-                   "WHERE BINARY name LIKE '{}' ORDER BY id ASC".format(state_name)
+    # Create the SQL query with user input
+    query = "SELECT * FROM states " \
+            "WHERE BINARY name LIKE '{}' ORDER BY id ASC".format(state_name)
 
     # Execute the query with user input
-    cursor.execute(query)    
-                  
-    # Fetch all rows from the result set
+    cursor.execute(query)
+
+    # Fetch all rows
     rows = cursor.fetchall()
 
     # Display the results
