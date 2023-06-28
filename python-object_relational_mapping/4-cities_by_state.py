897b3e4c13af7a67ed51dd82cb5aace354939611
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-27 18:18:46 -0700

    Create 4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""lists all cities from databse"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query to get all cities with state info
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                    JOIN states ON cities.state_id = states.id \
+                    ORDER BY cities.id ASC")
+
+    # Fetch rows
+    rows = cursor.fetchall()
+
+    # Display results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
